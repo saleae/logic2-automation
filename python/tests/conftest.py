e5b2ee00f4f7480cbce442d2153340775e7796fd
Ryan Huffman <ryanhuffman@gmail.com>
2022-09-06 10:46:58 -0700

    Add option to run automation tests with existing instance of app

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -4,12 +4,19 @@ import os.path
 
 import saleae.automation
 
+def pytest_addoption(parser):
+    parser.addoption('--use-existing', action='store_true')
+
 @pytest.fixture
 def asset_path() -> str:
     base_path = pathlib.Path(__file__).parent.resolve()
     return os.path.join(base_path, 'assets')
 
-@pytest.fixture
-def manager():
-    with saleae.automation.Manager(port=10430) as mgr:
-        yield mgr
+@pytest.fixture(scope='session')
+def manager(request):
+    if request.config.getoption('--use-existing'):
+        with saleae.automation.Manager(port=10430) as mgr:
+            yield mgr
+    else:
+        with saleae.automation.Manager.launch() as mgr:
+            yield mgr
