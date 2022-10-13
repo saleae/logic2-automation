4b05c690cf4c5fb5847d9cf634a8471261aaf1cf
Ryan Huffman <ryanhuffman@gmail.com>
2022-10-13 10:00:41 -0700

    Add support for generating protobufs in dev environment

--- python/grpc_build_hook.py
+++ python/grpc_build_hook.py
@@ -1,15 +1,23 @@
-from hatchling.builders.hooks.plugin.interface import BuildHookInterface
-import grpc_tools.protoc
+def build_protobufs(root_proto_dir: str):
+    import grpc_tools.protoc
+    import os.path
 
-class CustomBuildHook(BuildHookInterface):
-    def initialize(self, version, build_data):
-        print("Building logic2-automation protobuf files")
-        args = [
-            'protoc',
-            '-I', './proto',
-            '--python_out=./',
-            '--grpc_python_out=./',
-            './proto/saleae/grpc/saleae.proto'
-        ]
-        grpc_tools.protoc.main(args)
+    print("Building logic2-automation protobuf files")
+
+    args = [
+        'protoc',
+        '-I', root_proto_dir,
+        '--python_out=./',
+        '--grpc_python_out=./',
+        os.path.join(root_proto_dir, 'saleae/grpc/saleae.proto')
+    ]
+    grpc_tools.protoc.main(args)
+
+if __name__ == '__main__':
+    build_protobufs('../proto')
+else:
+    from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+    class CustomBuildHook(BuildHookInterface):
+        def initialize(self, version, build_data):
+            build_protobufs('./proto')
 
